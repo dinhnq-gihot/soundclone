4c2133d00c066920263d8b87a077ebab0467bfeb
dinhnq-gihot <dinh.nguyen@gihot.vn>
2024-06-06 18:08:29 +0700

    feat: handlers

---
+++ lib/service/src/handlers/user_handler.rs
@@ -0,0 +1,9 @@
+use {
+    actix_web::{get, http::StatusCode, post, web, HttpRequest, HttpResponse},
+    log::{debug, error},
+    soundclone_db::repositories::*,
+};
+
+async fn get(req: HttpRequest) {
+    
+}
